d0f99bd013cb475153c4270546a5289f7400edd5
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:49:46 +0000

    [automatic commit] 2025-06-20 01:49:45 UTC 124.184.141.100

--- part1/test.sql
+++ part1/test.sql
@@ -2,7 +2,7 @@ SELECT d.name, d.size, u.username
 FROM Dogs as d
 LEFT JOIN Users as u ON d.owner_id = u.user_id;
 
-SELECT r.request_id, d.name, r.requested_time, 
+SELECT r.request_id, d.name, r.requested_time, r.duration_minutes, r.location, u.username
 
 
 WHERE r.status = "open"
