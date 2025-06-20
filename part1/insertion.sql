c7587aabfe3be73d01bdd9717b7b0e7395771313
Echo1019 <hudson.u@hotmail.com>
2025-06-20 00:50:20 +0000

    [automatic commit] 2025-06-20 00:50:20 UTC 124.184.141.100

--- part1/insertion.sql
+++ part1/insertion.sql
@@ -1,7 +1,7 @@
 USING DogWalkService;
 
 INSERT INTO Users (username, email, password_hash, role) VALUES
-('john_dog', 'john@example.com', 'HASH1', 'walker'),
+('alice123', 'alice@example.com', 'hashed123', 'walker'),
 ('amy_123', 'amy@example.com', 'HASH2', 'owner'),
 ('mike99', 'mike@example.com', 'HASH3', 'walker');
 
