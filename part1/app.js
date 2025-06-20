d61d92d211d2cc27f26e73274de8ed6a1151e1f3
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:42:42 +0000

    [automatic commit] 2025-06-20 01:42:41 UTC 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -61,6 +61,14 @@ app.get("/api/dogs", async (req, res) => {
         ]
     */
     const [rows, fields] = await db.query('SELECT d.name, d.size, u.username FROM Dogs as d LEFT JOIN Users as u ON d.owner_id = u.user_id;');
+
+    const result = rows.map(row => ({
+        dog_name: row.name,
+        size: row.size,
+        owner_username: row.username
+    }));
+
+    res.json(result);
 });
 
 app.get("/api/walkrequests/open", async (req, res) => {
