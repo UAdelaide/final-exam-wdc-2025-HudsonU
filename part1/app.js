a69d805b12036e1bff4cf71a7c77b13b0f98de9f
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:52:23 +0000

    [automatic commit] 2025-06-20 01:52:22 UTC 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -88,7 +88,12 @@ app.get("/api/walkrequests/open", async (req, res) => {
             }
         ]
     */
-    const [rows] = await db.query('SELECT d.name, d.size, u.username FROM Dogs as d LEFT JOIN Users as u ON d.owner_id = u.user_id;');
+    const [rows] = await db.query(`
+        SELECT r.request_id, d.name, r.requested_time, r.duration_minutes, r.location, u.username
+        FROM WalkRequests AS r
+        JOIN Dogs AS d ON r.dog_id = d.dog_id
+        JOIN Users AS u ON d.owner_id = u.user_id
+        WHERE r.status = 'open';`);
 
     const result = rows.map((row) => ({
         dog_name: row.name,
