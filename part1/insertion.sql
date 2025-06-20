3339093a0afbfd7f38a9ffbe4e97100f61fd7775
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:00:45 +0000

    [automatic commit] 2025-06-20 01:00:45 UTC 124.184.141.100

--- part1/insertion.sql
+++ part1/insertion.sql
@@ -15,4 +15,8 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'timthedoglover'), 'Alakazam', 'large');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), "2025-06-10 08:00:00", 30, "Parklands", "open"),
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), "2025-06-10 08:00:00", 30, "Parklands", "open");
