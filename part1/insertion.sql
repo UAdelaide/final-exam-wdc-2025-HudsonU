e036c00429f220e4b8fbbf33ff0585be2c0ff11a
Echo1019 <hudson.u@hotmail.com>
2025-06-20 00:53:20 +0000

    [automatic commit] 2025-06-20 00:53:19 UTC 124.184.141.100

--- part1/insertion.sql
+++ part1/insertion.sql
@@ -8,5 +8,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('timthedoglover', 'tim@example.com', 'password456', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('alice123', 'alice@example.com', 'medium'),
 INSERT INTO WalkRequests
