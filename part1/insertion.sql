8808c4bc252f25503c13162d14999f116762605d
Echo1019 <hudson.u@hotmail.com>
2025-06-20 00:55:55 +0000

    [automatic commit] 2025-06-20 00:55:54 UTC 124.184.141.100

--- part1/insertion.sql
+++ part1/insertion.sql
@@ -11,7 +11,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'timthedoglover'), 'Abra', 'large'),
-((SELECT user_id FROM Users WHERE username = 'timethedoglover'), 'Kadabra', 'large'),
-((SELECT user_id FROM Users WHERE username = 'timethedoglover'), 'Alakazam', 'large');
+((SELECT user_id FROM Users WHERE username = 'timthedoglover'), 'Kadabra', 'large'),
+((SELECT user_id FROM Users WHERE username = 'timthedoglover'), 'Alakazam', 'large');
 
 INSERT INTO WalkRequests
