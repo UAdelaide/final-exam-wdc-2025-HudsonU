1cf5dcf69584feca1adf427146192f45ea09bc51
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:17:18 +0000

    [automatic commit] 2025-06-20 01:17:18 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -14,17 +14,17 @@ app.listen(3000, () => {
 app.get("/api/dogs", (req, res) => {
     // Return a list of all dogs with their size and owner's username.
     /* [
-    {
-        "dog_name": "Max",
-        "size": "medium",
-        "owner_username": "alice123"
-    },
-    {
-        "dog_name": "Bella",
-        "size": "small",
-        "owner_username": "carol123"
-    }
-    ]
+            {
+                "dog_name": "Max",
+                "size": "medium",
+                "owner_username": "alice123"
+            },
+            {
+                "dog_name": "Bella",
+                "size": "small",
+                "owner_username": "carol123"
+            }
+        ]
     */
 
 });
@@ -32,16 +32,16 @@ app.get("/api/dogs", (req, res) => {
 app.get("/api/walkrequests/open", (req, res) => {
     // Return all open walk requests, including the dog name, requested time, location, and owner's username
     /* [
-  {
-    "request_id": 1,
-    "dog_name": "Max",
-    "requested_time": "2025-06-10T08:00:00.000Z",
-    "duration_minutes": 30,
-    "location": "Parklands",
-    "owner_username": "alice123"
-  }
-]
-*/
+        {
+            "request_id": 1,
+            "dog_name": "Max",
+            "requested_time": "2025-06-10T08:00:00.000Z",
+            "duration_minutes": 30,
+            "location": "Parklands",
+            "owner_username": "alice123"
+        }
+        ]
+    */
 });
 
 app.get("/api/walkers/summary", (req, res) => {
