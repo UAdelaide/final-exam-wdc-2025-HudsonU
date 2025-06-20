b38f8af225a694ca68e46239a4200de7711e7143
Echo1019 <hudson.u@hotmail.com>
2025-06-20 06:23:35 +0000

    [automatic commit] 2025-06-20 06:23:35 UTC 124.184.141.100

--- part2/app.js
+++ part2/app.js
@@ -25,7 +25,7 @@ app.use('/api/users', userRoutes);
 
 app.get("/api/dogs", async (req, res) => {
     // Return a list of all dogs with their size and owner's username.
-    const [rows] = await db.query('SELECT d.dog_id, d.name as dog_name, d.size, u.username FROM Dogs as d LEFT JOIN Users as u ON d.owner_id = u.user_id;');
+    const [rows] = await db.query('SELECT d.dog_id, d.name as dog_name, d.size, u.owner_id FROM Dogs as d LEFT JOIN Users as u ON d.owner_id = u.user_id;');
 
     const result = rows.map((row) => ({
         dog_id: row.dog_id,
