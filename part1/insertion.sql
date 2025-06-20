855eaac45afb39affe429d907309069f3ec0c78d
Echo1019 <hudson.u@hotmail.com>
2025-06-20 02:06:53 +0000

    [automatic commit] 2025-06-20 02:06:53 UTC 124.184.141.100

--- part1/insertion.sql
+++ part1/insertion.sql
@@ -22,11 +22,11 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 ((SELECT dog_id FROM Dogs WHERE name = 'Alakazam'), "2025-06-13 08:00:00", 45, "Candylane Ln", "cancelled");
 
 INSERT INTO WalkApplications (request_id, walker_id, status) VALUES
-(1, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 'completed'),
+(1, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 'accepted'),
 (2, (SELECT user_id FROM Users WHERE username = 'johndoe'), 'accepted'),
 (3, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 'accepted'),
-(4, (SELECT user_id FROM Users WHERE username = 'johndoe'), 'pending'),
-(5, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 'rejected');
+(4, (SELECT user_id FROM Users WHERE username = 'johndoe'), 'accepted'),
+(5, (SELECT user_id FROM Users WHERE username = 'bobwalker'), 'accepted');
 
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
 (1, (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123'), 5, 'Great job! Max loved the walk.'),
