b2a820aa15046485b9536cb3c656c3b49e984c30
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:13:28 +0000

    [automatic commit] 2025-06-20 01:13:27 UTC 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -10,4 +10,7 @@ app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
 
+app.get("/api/dogs
+app.get("/api/walkrequests/open
+app.get("/api/walkers/summary
 module.exports = app;
