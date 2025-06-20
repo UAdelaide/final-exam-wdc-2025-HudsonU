b42d5b5dcbf7cb08564fcf2bd83735db417e4c19
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:17:51 +0000

    [automatic commit] 2025-06-20 01:17:50 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -32,14 +32,14 @@ app.get("/api/dogs", (req, res) => {
 app.get("/api/walkrequests/open", (req, res) => {
     // Return all open walk requests, including the dog name, requested time, location, and owner's username
     /* [
-        {
-            "request_id": 1,
-            "dog_name": "Max",
-            "requested_time": "2025-06-10T08:00:00.000Z",
-            "duration_minutes": 30,
-            "location": "Parklands",
-            "owner_username": "alice123"
-        }
+            {
+                "request_id": 1,
+                "dog_name": "Max",
+                "requested_time": "2025-06-10T08:00:00.000Z",
+                "duration_minutes": 30,
+                "location": "Parklands",
+                "owner_username": "alice123"
+            }
         ]
     */
 });
@@ -47,18 +47,19 @@ app.get("/api/walkrequests/open", (req, res) => {
 app.get("/api/walkers/summary", (req, res) => {
     // Return a summary of each walker with their average rating and number of completed walks.
     /* [
-  {
-    "walker_username": "bobwalker",
-    "total_ratings": 2,
-    "average_rating": 4.5,
-    "completed_walks": 2
-  },
-  {
-    "walker_username": "newwalker",
-    "total_ratings": 0,
-    "average_rating": null,
-    "completed_walks": 0
-  }
-] */
+            {
+                "walker_username": "bobwalker",
+                "total_ratings": 2,
+                "average_rating": 4.5,
+                "completed_walks": 2
+            },
+            {
+                "walker_username": "newwalker",
+                "total_ratings": 0,
+                "average_rating": null,
+                "completed_walks": 0
+            }
+        ]
+    */
 });
 module.exports = app;
