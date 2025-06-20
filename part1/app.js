e058c03c90fc68cdb3c83b64f7b63d27a68da2e3
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:14:33 +0000

    [automatic commit] 2025-06-20 01:14:32 UTC 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -11,11 +11,11 @@ app.listen(3000, () => {
 });
 
 app.get("/api/dogs", (req, res) => {
-
+    // Return a list of all dogs with their size and owner's username.
 });
 
 app.get("/api/walkrequests/open", (req, res) => {
-
+    // Return all open walk requests, including the dog name, requested time, location, and owner's username
 });
 
 app.get("/api/walkers/summary", (req, res) => {
