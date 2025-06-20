cb702e522732aa37e0857b25c6f74875838c2c29
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:57:05 +0000

    [automatic commit] 2025-06-20 01:57:05 UTC 124.184.141.100

--- part1/test.sql
+++ part1/test.sql
@@ -8,4 +8,14 @@ JOIN Dogs AS d ON r.dog_id = d.dog_id
 JOIN Users AS u ON d.owner_id = u.user_id
 WHERE r.status = 'open';
 
-SELECT w.username, count
+SELECT u.username AS walker_username, COUNT(DISTINCT wr.request_id) AS completed_walks, COUNT(r.rating_id) AS total_ratings, ROUND(AVG(r.rating), 1) AS average_rating
+FROM Users u
+LEFT JOIN WalkRequests wr ON u.user_id = (
+  SELECT wa.walker_id
+  FROM WalkApplications wa
+  WHERE wa.request_id = wr.request_id AND wa.status = 'accepted'
+  LIMIT 1
+)
+LEFT JOIN WalkRatings r ON u.user_id = r.walker_id AND wr.request_id = r.request_id
+WHERE u.role = 'walker' AND wr.status = 'completed'
+GROUP BY u.user_id, u.username;
