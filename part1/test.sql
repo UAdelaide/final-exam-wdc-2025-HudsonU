a89fae77d7b420d9debf496511c47b2d6a377219
Echo1019 <hudson.u@hotmail.com>
2025-06-20 01:59:37 +0000

    [automatic commit] 2025-06-20 01:59:37 UTC 124.184.141.100

--- part1/test.sql
+++ part1/test.sql
@@ -13,4 +13,4 @@ SELECT u.username AS walker_username, COUNT(r.rating_id) AS total_ratings,
 FROM Users u
 LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
 WHERE u.role = 'walker'
-GROUP BY u.user_id, u.username;
+GROUP BY u.user_id;
