a68c4d782aa3dc41bb41135edd532557f69cdf14
Echo1019 <hudson.u@hotmail.com>
2025-06-20 02:07:38 +0000

    [automatic commit] 2025-06-20 02:07:37 UTC 124.184.141.100

--- part1/app.js
+++ part1/app.js
@@ -125,11 +125,12 @@ app.get("/api/walkers/summary", async (req, res) => {
         ]
     */
     const [rows] = await db.query(`
-        SELECT r.request_id, d.name, r.requested_time, r.duration_minutes, r.location, u.username
-        FROM WalkRequests AS r
-        JOIN Dogs AS d ON r.dog_id = d.dog_id
-        JOIN Users AS u ON d.owner_id = u.user_id
-        WHERE r.status = 'open';`);
+    SELECT u.username AS walker_username, COUNT(r.rating_id) AS total_ratings,
+    ROUND(AVG(r.rating), 1) AS average_rating, COUNT(DISTINCT r.request_id) AS completed_walks
+    FROM Users u
+    LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+    WHERE u.role = 'walker'
+    GROUP BY u.user_id;`);
 
     const result = rows.map((row) => ({
         request_id: row.request_id,
